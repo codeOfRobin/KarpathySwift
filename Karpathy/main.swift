921fcb12dcdf7c9fd35628bc12cfd9bca83512d8
Robin Malhotra <malhotra.robin@icloud.com>
2016-03-28 02:49:52 +0530

    init

---
+++ Karpathy/main.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  Karpathy
+//
+//  Created by Robin Malhotra on 27/03/16.
+//  Copyright © 2016 Robin Malhotra. All rights reserved.
+//
+
+import Foundation
+
+//func forwardMultiplyGate(a:Float, b:Float)->Float
+//{
+//    return  a*b
+//}
+//
+//let x = Float(-2.0) , y = Float(-3.0)
+//let tweakAmount = Float(0.01)
+//var best_out = Float(-99999999999999)
+//
+//var best_x = x, best_y = y
+//for k in 0..<10000000000
+//{
+//    var x_try = x + tweakAmount * (Float(rand()) / Float(UINT32_MAX) * 2 - 1)
+//    var y_try = y + tweakAmount * (Float(rand()) / Float(UINT32_MAX) * 2 - 1)
+//    let out = forwardMultiplyGate(x_try, b: y_try)
+//    if out > best_out
+//    {
+//        best_out = out
+//        best_x = x_try
+//        best_y = y_try
+//    }
+//}
+//print(best_out)
+
+
